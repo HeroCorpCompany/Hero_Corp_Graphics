9a93d86fae043b48a6cdb998799ec30e8f057dfa
alex.goussian@gmail.com <alex.goussian@gmail.com>
2021-06-07 12:53:47 +0200

    Implemetation of Engine

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -1,10 +1,50 @@
 #include "Engine.h"
 
+const sf::Time Engine::m_timePerFrame = sf::seconds(0.00001);
+
 Engine::Engine()
+: m_window(sf::VideoMode(650, 650), "Random Noise")
 {
 }
 
 void Engine::run()
 {
-    std::cout << "Hello World" << std::endl;
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+
+	while (m_window.isOpen())
+	{
+		processEvents();
+		timeSinceLastUpdate += clock.restart();
+		
+		while (timeSinceLastUpdate > m_timePerFrame)
+		{
+			timeSinceLastUpdate -= m_timePerFrame;
+			update();
+		}
+
+		render();
+	}
+}
+
+void Engine::processEvents()
+{
+	sf::Event event;
+	while (m_window.pollEvent(event))
+	{
+		if (event.type == sf::Event::Closed)
+			m_window.close();
+	}
+}
+
+void Engine::update()
+{
+    // What you want to update
+}
+
+void Engine::render()
+{
+	m_window.clear();
+	// What you want to display
+	m_window.display();
 }
--- src/engine/Engine.h
+++ src/engine/Engine.h
@@ -1,13 +1,24 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-class Engine {
+class Engine
+{
 public :
-    Engine();
+	
+	Engine();
+	void run();
 
-    void run();
+private :
+	
+	void processEvents();
+	void update();
+	void render();
+	
+private :
+	sf::RenderWindow m_window;
+	static const sf::Time m_timePerFrame;
 };
 
-#endif // ENGINE_H
+#endif //ENGINE_H
